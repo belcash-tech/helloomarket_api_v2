c5ddb3aeebb35ef46feec099620721e4e187c6e1
Yared Getachew <yared.getachew.tessema@gmail.com>
2020-01-23 12:42:11 +0300

    Type casting in products

--- products.js
+++ products.js
@@ -15,6 +15,7 @@ const _self = (module.exports = {
     await categories.filter(category => {
       if (category.category_id === category_id) {
         category_response = category;
+	category_response.category_id = parseInt(category_response.category_id)
         return category_response;
       }
     });
@@ -27,10 +28,15 @@ const _self = (module.exports = {
       if (category.category_id === category_id) {
         category_products = category;
         category_products.products.map(k => {
+	  k.product_id = parseInt(k.product_id)
+	  k.quantity = parseInt(k.quantity)
+	  k.manufacturer_id = parseInt(k.manufacturer_id)
+	  k.category_id = parseInt(category.category_id)
           k.thumbnail = [_base_url, 'media/300', k.image.split('/')[1]].join(
             '/'
           );
         });
+	category_products.category_id = parseInt(category_products.category_id)
         return category_products;
       }
     });
@@ -58,6 +64,10 @@ const _self = (module.exports = {
     let products = await _self.objectifier(categories);
     if (products.hasOwnProperty(product_id)) {
       let product = products[product_id];
+      product.product_id = parseInt(product.product_id)
+      product.category_id = parseInt(product.category_id)
+      product.manufacturer_id = parseInt(product.manufacturer_id)
+      product.quantity = parseInt(product.quantity)
       product.thumbnail = product.image.split('/')[1];
       product.thumbnail = [_base_url, 'media/300', product.thumbnail].join('/');
       return product;
@@ -75,6 +85,8 @@ const _self = (module.exports = {
         p.category_id = parseInt(pg.category_id);
         p.product_id = parseInt(p.product_id);
         pg.category_id = parseInt(pg.category_id);
+	p.manufacturer_id = parseInt(p.manufacturer_id);
+	p.quantity = parseInt(p.quantity)
         p.category = pg;
         p.thumbnail = [_base_url, 'media/300', p.image.split('/')[1]].join('/');
       });
